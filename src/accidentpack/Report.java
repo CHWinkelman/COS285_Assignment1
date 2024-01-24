d249eaed37aa30861973a96ef243e47726be9171
Charles Winkelman <charles@chw.ink>
2024-01-23 23:17:13 -0500

    Added skeleton to run program1

--- src/accidentpack/Report.java
+++ src/accidentpack/Report.java
@@ -3,9 +3,25 @@
  */
 package accidentpack;
 
+import java.time.LocalDateTime;
+
 /**
- * 
+ * Object representing an accident report
+ * @
  */
 public class Report {
 
+	private String 			id;
+	private int 			severity;
+	private LocalDateTime 	startTime;
+	private LocalDateTime 	endTime;
+	private Address			location;
+	private int 			tempurature;
+	private int 			huimidity;
+	private int 			visibility;
+	private String 			weather;
+	private Boolean 		crossing;
+	private String			timeOfDay;
+	
+	
 }
